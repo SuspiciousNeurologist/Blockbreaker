39ffef9a9f91a510e38205e96b0754ccf7f003b0
AndreW <aldape.andres2@gmail.com>
2019-08-31 19:24:39 -0500

    First Commit

---
+++ Assets/Scripts/Block.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+    // config params
+    [SerializeField] AudioClip breakSound;
+    [SerializeField] GameObject blockVFX; 
+    [SerializeField] int maxHits;
+    [SerializeField] Sprite[] blockState;
+
+    //cached reference
+    Level level;
+    GameStatus gameStatus;
+
+    //state variables
+    [SerializeField] int timesHit = 0 ; //Serialized just for debugging.
+
+
+    //Block meta
+   
+    private void  OnCollisionEnter2D(Collision2D collision)
+    {
+        if(tag == "Breakable")
+        {
+            HandleHit();
+        }
+    }
+
+    private void HandleHit()
+    {
+        timesHit++;
+        if(timesHit >= maxHits)
+            {
+            DestroyBlock();
+        }
+        else
+        {
+            ShowNextHitSprite();
+        }
+    }
+
+    private void ShowNextHitSprite()
+    {
+        int spriteIndex = timesHit;
+        if(blockState[spriteIndex]!=null)
+        {
+            GetComponent<SpriteRenderer>().sprite = blockState[spriteIndex];
+        }else{
+            Debug.LogError("Block Sprite is missing in "+gameObject.name+".");
+        } 
+    }
+
+    private void DestroyBlock()
+    {
+        AudioSource.PlayClipAtPoint(breakSound,Camera.main.transform.position);
+        level.BlockDestroyed();
+        gameStatus.AddToScore();
+        Destroy(gameObject);
+        TriggerSparklesVFX();
+        
+    }
+
+    private void TriggerSparklesVFX()
+    {
+        GameObject sparkles = Instantiate(blockVFX,transform.position,transform.rotation);
+        Destroy(sparkles,2f);
+    }
+
+    private void CountBreakableBlocks()
+    {
+        level = FindObjectOfType<Level>();
+        if(tag == "Breakable")
+        {
+            level.CountBreakableBlocks();
+        }
+    }
+
+    private void Start()
+    {
+        CountBreakableBlocks();
+        gameStatus = FindObjectOfType<GameStatus>();
+    }
+
+    private void Update(){
+        if(Input.GetKeyDown(KeyCode.F)){
+            level.BlockDestroyed();
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/GameStatus.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameStatus : MonoBehaviour
+{
+    // config paramaters
+    [Range(0.1f,10f)][SerializeField] float gameSpeed = 1f;
+    [SerializeField] int pointsPerBlock = 83;
+    [SerializeField] TextMeshProUGUI scoreText;
+    [SerializeField] bool isAutoplayEnabled;
+
+    //state var
+    [SerializeField] int currentScore = 0;
+    
+
+
+    void Awake()
+    {
+        int gameStatusCount = FindObjectsOfType<GameStatus>().Length;
+        if (gameStatusCount > 1)
+        {
+            gameObject.SetActive(false);
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    void Start()
+    {
+        scoreText.text = currentScore.ToString();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        Time.timeScale = gameSpeed;
+    }
+
+    public void AddToScore()
+    {
+        currentScore += pointsPerBlock; 
+        scoreText.text = currentScore.ToString();
+    }
+
+    public void ResetGame()
+    {
+        Destroy(gameObject);
+    }
+
+    public bool IsAutoplayEnabled()
+    {
+        return isAutoplayEnabled;
+    }
+}
